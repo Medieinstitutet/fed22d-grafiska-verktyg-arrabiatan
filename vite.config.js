3ab796cf03d5ca79900c3f3d57634d8b97fe28e6
CeciliaL1 <cecilia.lepik@hotmail.com>
2023-01-14 12:16:02 +0100

    add vite config and script

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: 'https://github.com/Medieinstitutet/fed22d-grafiska-verktyg-arrabiatan',
+  define: {
+    'process.env': {},
+  },
+});
