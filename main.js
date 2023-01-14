1a415e870a16cbc825e531eded3b5d6bc9e272c8
CeciliaL1 <cecilia.lepik@hotmail.com>
2023-01-14 12:46:28 +0100

    change animation to menu-options

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ const spanHamOne = document.querySelector('#hamSpanOne');
 const spanHamTwo = document.querySelector('#hamSpanTwo');
 const spanHamThree = document.querySelector('#hamSpanThree');
 const menu = document.querySelector('#hamMenu');
-const menuOpen = document.querySelector('.ham-btn-menu');
-console.log(menuOpen)
+const openMenuOption = document.querySelector('#open-menu-options');
 
 let checkState = true;
 
@@ -31,7 +30,7 @@ function hamburgerMenu(){
         gsap.to(spanHamThree, {y: -20, rotate: -50, duration: 1});
         checkState = false;
         menu.classList.add('menu-open');
-        gsap.fromTo(menuOpen, {y: -500, duration: 3}, {y: 0, duration: 3})
+        gsap.fromTo(openMenuOption, {y: -500, duration: 2}, {y: 0, duration: 2})
            
     }else if (checkState === false){
         gsap.to(spanHamTwo, { backgroundColor: '#F59C30',border: '1px solid black', duration: 2 });
